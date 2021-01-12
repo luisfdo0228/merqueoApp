5a592947d648c11d961bee6be96f5608069c31e0
Luis Fernando Hernandez <hluisfernando1@gmail.com>
2021-01-12 14:07:53 -0500

    organizando diseño responsive

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDpVcmA3CDnMr-OUuKJhhUCeQGwcf2t-08",
+    authDomain: "publicaciones-merqueo.firebaseapp.com",
+    projectId: "publicaciones-merqueo",
+    storageBucket: "publicaciones-merqueo.appspot.com",
+    messagingSenderId: "51741673116",
+    appId: "1:51741673116:web:834a6e608c387979f38f59"
+  }
 };
